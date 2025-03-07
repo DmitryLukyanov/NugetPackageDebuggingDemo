ffb3f0ad345b3fb2eb337aa3ddbe15b0e25e8c14
DmitryLukyanov <dzima.lukyanau@gmail.com>
2025-03-07 17:20:43 +0400

    +1 test

---
+++ NugetPackageDebuggingLegact452FrameworkDemoWithSourceLinkV2/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetPackageDebuggingLegact452FrameworkDemoWithSourceLinkV2
+{
+    public class DebuggingTestLegacyFramework461
+    {
+        public void Test()
+        {
+        }
+
+        public void TestWithDebugger()
+        {
+            Debugger.Break();
+        }
+    }
+}
